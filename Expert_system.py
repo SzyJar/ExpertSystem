a306c219bf8b920c848b386b58e6ecf04a377c65
Szymon Jaroszyński <szymjare@gmail.com>
2022-10-18 18:36:41 +0200

    some work

--- Expert_system.py
+++ Expert_system.py
@@ -2,19 +2,30 @@ import tkinter as tk
 from tkinter import filedialog, Text
 import os
 
+root = tk.Tk()
+root.title("System ekspercki - silniki elektryczne")
+root.resizable(width=False, height=False)
 
-root =tk.Tk(screenName = "System ekspercki")
-
-def exitApp():
+def exitAppCommand():
     quit()
 
 canvas = tk.Canvas(root, height=500, width=500, bg ="#ffffff")
 canvas.pack()
 
-frame=tk.Frame(root, bg="#33001a")
+#################################################   frames
+frame=tk.Frame(root, bg="#d0d0e1")
 frame.place(relwidth=0.9,relheight=0.9, relx=0.05, rely=0.05)
 
-exitApp = tk.Button(root, text="Wyjscie", padx=10, pady=10, fg="white", bg = "black", command=exitApp)
+exitAppFrame=tk.Frame(root, bg="#33001a")
+exitAppFrame.place(relwidth=0.1,relheight=0.07, relx=0.8, rely=0.8)
+
+nextQFrame=tk.Frame(root, bg="#33001a")
+nextQFrame.place(relwidth=0.2,relheight=0.07, relx=0.4, rely=0.8)
+##############################################   Buttons
+exitApp = tk.Button(exitAppFrame, text="Wyjscie", padx=10, pady=10, fg="black", bg = "#cccccc", command=exitAppCommand)
 exitApp.pack()
+
+nextQ = tk.Button(nextQFrame, text="Nastepne pytanie", padx=10, pady=10, fg="black", bg = "#cccccc")
+nextQ.pack()
 
 root.mainloop()

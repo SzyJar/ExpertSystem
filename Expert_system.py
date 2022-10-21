a4c21daaa75e4431c3e09fa0fdb2a91a774112f8
Szymon Jaroszyński <szymjare@gmail.com>
2022-10-21 19:33:15 +0200

    More work on TK

--- Expert_system.py
+++ Expert_system.py
@@ -1,31 +1,33 @@
 import tkinter as tk
-from tkinter import filedialog, Text
-import os
 
 root = tk.Tk()
 root.title("System ekspercki - silniki elektryczne")
 root.resizable(width=False, height=False)
 
-def exitAppCommand():
-    quit()
-
 canvas = tk.Canvas(root, height=500, width=500, bg ="#ffffff")
 canvas.pack()
+#############################################  results frame
 
-#################################################   frames
+def results():
+    resultsFrame=tk.Frame(root, bg="#d0d0e1")
+    resultsFrame.place(relwidth=0.9,relheight=0.9, relx=0.05, rely=0.05)
+    buttonFrame=tk.Frame(resultsFrame, bg="#d0d0e1")
+    buttonFrame.place(relwidth=0.2, relheight=0.07, relx=0.4, rely=0.8)
+    def goBack():
+        resultsFrame.destroy()
+    backButton = tk.Button(buttonFrame, text="Powrot", padx=10, pady=10, fg="black", bg = "#cccccc", command=goBack)
+    backButton.pack()
+    
+################################################# main frame
+
 frame=tk.Frame(root, bg="#d0d0e1")
 frame.place(relwidth=0.9,relheight=0.9, relx=0.05, rely=0.05)
 
-exitAppFrame=tk.Frame(root, bg="#33001a")
-exitAppFrame.place(relwidth=0.1,relheight=0.07, relx=0.8, rely=0.8)
+nextQFrame=tk.Frame(root, bg="#d0d0e1")
+nextQFrame.place(relwidth=0.2, relheight=0.07, relx=0.4, rely=0.8)
 
-nextQFrame=tk.Frame(root, bg="#33001a")
-nextQFrame.place(relwidth=0.2,relheight=0.07, relx=0.4, rely=0.8)
-##############################################   Buttons
-exitApp = tk.Button(exitAppFrame, text="Wyjscie", padx=10, pady=10, fg="black", bg = "#cccccc", command=exitAppCommand)
-exitApp.pack()
+resultsButton = tk.Button(nextQFrame, text="Wyniki", padx=10, pady=10, fg="black", bg = "#cccccc", command=results)
+resultsButton.pack()
 
-nextQ = tk.Button(nextQFrame, text="Nastepne pytanie", padx=10, pady=10, fg="black", bg = "#cccccc")
-nextQ.pack()
-
+############################################
 root.mainloop()
